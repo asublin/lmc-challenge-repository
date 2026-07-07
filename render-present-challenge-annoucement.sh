8cd832cb0684c3e72120da63a775ca9bbccbc13e
ASubLin <asublin@gmail.com>
2026-07-07 21:04:23 +0300

    Complete (or almost) rendering present challenge annoucement

--- render-present-challenge-annoucement.sh
+++ render-present-challenge-annoucement.sh
@@ -10,13 +10,18 @@ source common.sh
 
 present_challenge="$(get_present_challenge)"
 lmc_round=$(get_lmc_round "${present_challenge}")
-lmc_title=$(get_lmc_title "${present_challenge}")
+lmc_title="$(get_lmc_title "${present_challenge}")"
 lmc_start_date=$(get_lmc_start_date "${present_challenge}")
 lmc_end_date=$(get_lmc_end_date "${present_challenge}")
+lmc_short_description="$(get_lmc_short_description "${present_challenge}")"
+lmc_long_description="$(get_lmc_long_description "${present_challenge}")"
 
-echo "Title:"
-echo "Libre Music Challenge #${lmc_idx} ${lmc_title}"
+echo "Subject:"
+echo "Libre Music Challenge #${lmc_round} ${lmc_title}"
 echo
 echo "Body:"
-echo "Welcome to the Libre Music Challenge #${lmc_idx}!"
-#NEXT
+echo "Welcome to the Libre Music Challenge #${lmc_round}!"
+echo
+echo "[b]${lmc_short_description}[/b]"
+echo
+pandoc --from=markdown <<< "$lmc_long_description" --to=pandoc-bbcode-phpbb/phpbb.lua
